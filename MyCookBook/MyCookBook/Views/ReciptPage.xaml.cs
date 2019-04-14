a8126fdb9f89ac48de66953efaf9e8f9e2cfe34d
MariuszRukowicz <mariuszrukoiwcz@wp.pl>
2019-04-14 19:21:42 +0200

    add recipt page

---
+++ MyCookBook/MyCookBook/Views/ReciptPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MyCookBook.Models;
+using MyCookBook.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCookBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ReciptPage : ContentPage
+	{
+		public ReciptPage (Recipe recipe)
+		{
+			InitializeComponent ();
+            BindingContext = new RecipeDetailsViewModel(recipe);
+        }
+	}
+}
