a37b48828010952ef82264d1d35e96097744ebdd
MariuszRukowicz <mariuszrukoiwcz@wp.pl>
2019-04-14 19:20:12 +0200

    add list page and search page

---
+++ MyCookBook/MyCookBook/Views/ListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MyCookBook.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCookBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListPage : ContentPage
+	{
+		public ListPage ()
+		{
+			InitializeComponent ();
+            BindingContext = new RecipeViewModel();
+		}
+	}
+}
---
+++ MyCookBook/MyCookBook/Views/SearchPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MyCookBook.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCookBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchPage : ContentPage
+	{
+		public SearchPage()
+		{
+			InitializeComponent ();
+            BindingContext = new SearchViewModel();
+		}
+	}
+}
