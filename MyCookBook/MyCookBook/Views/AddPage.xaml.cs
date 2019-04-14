1f4f2b8869a9f744d855c174efe93ec80aec6d83
MariuszRukowicz <mariuszrukoiwcz@wp.pl>
2019-04-14 19:22:42 +0200

    add edit page and add Page

---
+++ MyCookBook/MyCookBook/Views/AddPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MyCookBook.ViewModels;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCookBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddPage : ContentPage
+	{
+        public AddPage()
+        {
+            InitializeComponent();
+            BindingContext = new AddViewModel();
+        }           
+    }
+}
